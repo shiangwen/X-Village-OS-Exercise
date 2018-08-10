949199622260f5844d1f0f98ce35bebd3fb73a2c
shiangwen <40990722+shiangwen@users.noreply.github.com>
2018-08-11 07:35:23 +0800

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,39 @@
+import threading
+import queue
+import os
+
+buffer_size = 5
+
+lock = threading.Lock()
+queue = queue.Queue(buffer_size)
+file_count = 0
+
+def producer(top_dir, queue_buffer):
+    # Search sub-dir in top_dir and put them in queue
+    pass
+
+def consumer(queue_buffer):
+    # search file in directory
+    pass
+
+
+def main():
+    producer_thread = threading.Thread(target = producer, args = ('./testdata', queue))
+
+    consumer_count = 20
+    consumers = []
+    for i in range(consumer_count):
+        consumers.append(threading.Thread(target = consumer, args = (queue,)))
+
+    producer_thread.start()
+    for c in consumers:
+        c.start()
+
+    producer_thread.join()
+    for c in consumers:
+        c.join()
+
+    print(file_count, 'files found.')
+
+if __name__ == "__main__":
+    main()
